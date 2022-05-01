4ea5a294ca62aeb5c84d12a77c636825ed36db12
Anhdao153 <ndthien.1532000@gmail.com>
2022-05-01 16:21:30 +0700

    authenrization

---
+++ src/main/java/com/example/demo/dto/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String userName;
+    private String passWord;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roleCollection = new ArrayList<>();
+
+
+}
